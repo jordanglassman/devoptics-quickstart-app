05d204d8a0d26c38796905dc020a7d313fb78080
Jordan Glassman <jglassman@cloudbees.com>
2019-09-30 14:15:16 -0400

    TEST-11

--- project-b/src/main/java/devoptics/App.java
+++ project-b/src/main/java/devoptics/App.java
@@ -3,5 +3,8 @@ package devoptics;
 public class App {
     public static void main(String[] args) {
         System.out.println("fdfdf");
+        System.out.println("fdfdf");
+        System.out.println("fddsadfdf");
+        System.out.println("fdfdf");
     }
 }
